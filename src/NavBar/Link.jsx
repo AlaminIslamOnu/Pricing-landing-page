5328f1c221ebc9d8339aa1edffa9bde931307bca
AlaminIslamOnu <alamin.ndc.17@gmail.com>
2025-04-13 10:38:34 +0600

    navbar done

--- src/NavBar/Link.jsx
+++ src/NavBar/Link.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Link = ({route}) => {
-    
-    return (
-        <li className='mr-12 font-bold'>
-            {<a href={route.path}>{route.name}</a> }
-        </li>
-    );
+const Link = ({ route }) => {
+  return (
+    <li className="mr-12 font-bold ">
+      {<a href={route.path}>{route.name}</a>}
+    </li>
+  );
 };
-   
+
 export default Link;
--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const navigationData = [
-    { id: 1, name: "Home", path: "/" },
-    { id: 2, name: "About", path: "/about" },
-    { id: 3, name: "Services", path: "/services" },
-    { id: 4, name: "Blog", path: "/blog" },
-    { id: 5, name: "Contact", path: "/contact" }
-  ];
-  
+  { id: 1, name: "Home", path: "/" },
+  { id: 2, name: "About", path: "/about" },
+  { id: 3, name: "Services", path: "/services" },
+  { id: 4, name: "Blog", path: "/blog" },
+  { id: 5, name: "Contact", path: "/contact" },
+];
+
 const Navbar = () => {
-    return (
-        <div>
-      <ul className='flex justify-between'>
-        { 
-            navigationData.map(route=>  <Link route={route} key={route.id}> </Link>)
-        }
+  const [open,setOpen] = useState(false)
+  const links= navigationData.map((route) => (
+    <Link route={route} key={route.id}>
+      {" "}
+    </Link>
+  ))
+
+  return (
+    <nav className="flex justify-between mx-10">
+      <span className="flex" onClick={()=> setOpen(!open)}>
+        { open ? <X className="md:hidden"> </X> :  <Menu className="md:hidden"> </Menu>}
+       <ul className="md:hidden"> 
+       {links}
+       </ul>
+       <h3 className="ml-4">Mynav</h3>
+      </span>
+      <ul className="md:flex hidden">
+       {links}
       </ul>
-            
-        </div>
-    );
+      <button>Sing in</button>
+    </nav>
+  );
 };
 
 export default Navbar;
